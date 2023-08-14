a213083632bb38fe5d20beb5f233769ee48a9ef5
Grace0615 <graceebruwiyor@gmail.com>
2023-08-14 22:54:04 +0100

    7-print_tebahplaz

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Description - this reverses something
+ *  Return: 0
+ */
+
+int main(void)
+{
+		char d;
+
+			for (d = 'z'; d >= 'a'; d--)
+			{
+			putchar(d);
+			}
+				putchar('\n');
+					return (0);
+}
