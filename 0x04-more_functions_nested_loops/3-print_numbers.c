83cf5b275318d06abd106d2ccad408f832be702e
Grace0615 <graceebruwiyor@gmail.com>
2023-08-17 23:58:37 +0100

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,17 +2,17 @@
 
 /**
  * print_numbers - print 0-9
- * Return: void
+ * Return: 0
  */
 
 void print_numbers(void)
 {
 	char c;
+
 	for (c = '0'; c <= '9'; c++)
 	{
 	_putchar(c);
 	}
 	_putchar('\n');
-	
 	return (0);
 }
