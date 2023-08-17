e66b5162db161a2eb9c78881ff89fef50217dd53
Grace0615 <graceebruwiyor@gmail.com>
2023-08-18 00:07:09 +0100

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,11 +2,12 @@
 
 /**
  * print_numbers - print 0-9
- * Return: 0
+ * return: void
  */
 
 void print_numbers(void)
 {
+
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
@@ -14,5 +15,4 @@ void print_numbers(void)
 	_putchar(c);
 	}
 	_putchar('\n');
-	return (0);
 }
